7e34b8a4849cbff3b0821e29c08bf0e196f240b7
jinfeng <851406283@qq.com>
2021-07-02 17:55:13 +0800

    submit: topic-23：Convert Sorted Array to Binary Search Tree

---
+++ topics/topic-23：Convert Sorted Array to Binary Search Tree/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// 将有序数组转换为二叉搜索树
+
+ public class TreeNode {
+      public var val: Int
+      public var left: TreeNode?
+      public var right: TreeNode?
+      public init() { self.val = 0; self.left = nil; self.right = nil; }
+      public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+      public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+          self.val = val
+          self.left = left
+          self.right = right
+      }
+  }
+
+
+func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
+
+}
