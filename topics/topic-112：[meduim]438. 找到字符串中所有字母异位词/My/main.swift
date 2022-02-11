4e0b790d78be47ced0a77be5d80e0b91c8cfef83
jinfeng <851406283@qq.com>
2022-02-11 11:39:20 +0800

    submit: topic-112：[meduim]438. 找到字符串中所有字母异位词

---
+++ topics/topic-112：[meduim]438. 找到字符串中所有字母异位词/My/main.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+// 438. 找到字符串中所有字母异位词
+
+class Solution {
+    func findAnagrams(_ s: String, _ p: String) -> [Int] {
+        // 滑动窗口
+    }
+}
+
+
+let s = Solution()
+let res = s.findAnagrams("cbaebabacd", "abc")
+print(res)
