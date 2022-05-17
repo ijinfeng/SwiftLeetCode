37b09335fcd85fd2d04bee717c60325acb869299
jinfeng <851406283@qq.com>
2022-05-17 11:08:20 +0800

    submit: topic-117：[easy]953. 验证外星语词典

--- topics/topic-117：[easy]953. 验证外星语词典/My/main.swift
+++ topics/topic-117：[easy]953. 验证外星语词典/My/main.swift
@@ -1,2 +1,45 @@
 import Foundation
 
+
+class Solution {
+    
+    
+    
+    func isAlienSorted(_ words: [String], _ order: String) -> Bool {
+        var orderMap: [Character: Int] = [:]
+        for i in 0..<order.count  {
+            let c = order[order.index(order.startIndex, offsetBy: i)]
+            orderMap[c] = i
+        }
+        
+        print(orderMap)
+        
+        for i in 0..<words.count-1 {
+            let first = words[i]
+            let second = words[i+1]
+            var ptr = 0
+            var sorted = false
+            while (ptr < first.count && ptr < second.count) {
+                let fc = first[first.index(first.startIndex, offsetBy: ptr)]
+                let sc = second[second.index(second.startIndex, offsetBy: ptr)]
+                if (orderMap[fc]! > orderMap[sc]!) {
+                    return false
+                } else if (orderMap[fc]! < orderMap[sc]!) {
+                    sorted = true
+                    break
+                }
+                ptr += 1
+            }
+            if (!sorted && first.count > second.count) {
+                return false
+            }
+        }
+        return true
+    }
+    
+
+}
+
+let s = Solution()
+let ret = s.isAlienSorted(["apple","app"], "abcdefghijklmnopqrstuvwxyz")
+print(ret)
